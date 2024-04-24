56158992d0729084462a8f670034c6e176ae8bba
Nguyen Tan Khoi <khoitony35@gmail.com>
2024-04-24 20:26:53 +0700

    feat: add course controller

--- EngLine/Controllers/HomeController.cs
+++ EngLine/Controllers/HomeController.cs
@@ -1,37 +1,67 @@
 using EngLine.Models;
+using EngLine.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace EngLine.Controllers
 {
-	public class HomeController : Controller
-	{
-		private readonly ILogger<HomeController> _logger;
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IUserRepository _userRepository;
+        private readonly IPaymentRepository _paymentRepository;
+        private readonly ICourseRepository _courseRepository;
+        private readonly IClassRepository _classRepository;
 
-		public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
-		}
+        public HomeController(
+            ILogger<HomeController> logger,
+            IUserRepository userRepository,
+            IPaymentRepository paymentRepository,
+            ICourseRepository courseRepository,
+            IClassRepository classRepository)
+        {
+            _logger = logger;
+            _userRepository = userRepository;
+            _paymentRepository = paymentRepository;
+            _courseRepository = courseRepository;
+            _classRepository = classRepository;
+        }
 
-		public IActionResult Index()
-		{
-			return View();
-		}
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-		public IActionResult HelloWorld()
-		{
-			return View();
-		}
+        public IActionResult HelloWorld()
+        {
+            return View();
+        }
 
-		public IActionResult Privacy()
-		{
-			return View();
-		}
+        public IActionResult Privacy()
+        {
+            return View();
+        }
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+        public async Task<IActionResult> Course()
+        {
+            var courses = await _courseRepository.GetAllAsync();
+
+            if (courses == null || !courses.Any())
+            {
+                ViewBag.IsEmpty = true;
+            }
+            else
+            {
+                ViewBag.IsEmpty = false;
+            }
+
+            return View(courses);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
 }
