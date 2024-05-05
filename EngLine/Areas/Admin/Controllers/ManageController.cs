529105eb49e1c548b924b7a135c6ec4cca677f40
tuantran <146686124+Tuna1423@users.noreply.github.com>
2024-05-05 20:14:54 +0700

    feat: dashboard admin

---
+++ EngLine/Areas/Admin/Controllers/ManageController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EngLine.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[Authorize]
+	public class ManageController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
