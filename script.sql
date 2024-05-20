7d7ca3adb071f9379643067b6610adc2d72b5ef5
Nguyen Tan Khoi <tankhoi46@gmail.com>
2024-05-20 14:55:39 +0700

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,29 +1,33 @@
 USE DBDACS;
+DBCC CHECKIDENT ('Tests', RESEED, 0);
+DBCC CHECKIDENT ('Questions', RESEED, 0);
+DBCC CHECKIDENT ('AnswerOptions', RESEED, 0);
+
 -- Insert into tests table
-INSERT INTO tests (id, title)
-VALUES (1, 'Sample Test 1'),
-    (2, 'Sample Test 2');
+INSERT INTO Tests (Title, TimeLimit)
+VALUES ('Sample Test 1', '2024-05-20 10:00:00'),
+    ('Sample Test 2', '2024-05-20 11:00:00');
 -- Insert into questions table
-INSERT INTO questions (id, content, TestId)
-VALUES (1, 'What is the capital of France?', 1),
-    (2, 'What is 2 + 2?', 1),
-    (3, 'Who wrote "Hamlet"?', 2),
-    (4, 'What is the boiling point of water?', 2);
+INSERT INTO Questions (Content, TestId, Point)
+VALUES ('What is the capital of France?', 1, 10),
+    ('What is 2 + 2?', 1, 10),
+    ('Who wrote "Hamlet"?', 2, 10),
+    ('What is the boiling point of water?', 2, 10);
 -- Insert into answer_options table
-INSERT INTO AnswerOptions(id, content, QuestionId, IsCorrectOption)
-VALUES (1, 'Paris', 1, 1),
-    (2, 'London', 1, 0),
-    (3, 'Berlin', 1, 0),
-    (4, 'Madrid', 1, 0),
-    (5, '3', 2, 0),
-    (6, '4', 2, 1),
-    (7, '5', 2, 0),
-    (8, '6', 2, 0),
-    (9, 'William Shakespeare', 3, 1),
-    (10, 'Charles Dickens', 3, 0),
-    (11, 'Mark Twain', 3, 0),
-    (12, 'Jane Austen', 3, 0),
-    (13, '90°C', 4, 0),
-    (14, '100°C', 4, 1),
-    (15, '110°C', 4, 0),
-    (16, '120°C', 4, 0);
+INSERT INTO AnswerOptions(Content, QuestionId, IsCorrectOption)
+VALUES ('Paris', 1, 1),
+    ('London', 1, 0),
+    ('Berlin', 1, 0),
+    ('Madrid', 1, 0),
+    ('3', 2, 0),
+    ('4', 2, 1),
+    ('5', 2, 0),
+    ('6', 2, 0),
+    ('William Shakespeare', 3, 1),
+    ('Charles Dickens', 3, 0),
+    ('Mark Twain', 3, 0),
+    ('Jane Austen', 3, 0),
+    ('90°C', 4, 0),
+    ('100°C', 4, 1),
+    ('110°C', 4, 0),
+    ('120°C', 4, 0);
