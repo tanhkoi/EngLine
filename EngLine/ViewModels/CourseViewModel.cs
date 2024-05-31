a89a0f19ff22c7c61c4dc012ec29f9c0466d9dc6
Nguyen Tan Khoi <tankhoi46@gmail.com>
2024-05-31 09:33:15 +0700

    feat: feature study

---
+++ EngLine/ViewModels/CourseViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EngLine.ViewModels
+{
+	public class CourseViewModel
+	{
+		public int CourseId { get; set; }
+		public string CourseName { get; set; }
+		public string Description { get; set; }
+		public string CoverPhoto { get; set; }
+		public ICollection<LessonViewModel> Lessons { get; set; }
+	}
+}
---
+++ EngLine/ViewModels/LessonViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EngLine.ViewModels
+{
+	public class LessonViewModel
+	{
+		public int LessonId { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string? Photo { get; set; }
+		public string? Video { get; set; }
+		public string Content { get; set; }
+	}
+}
